0f1e78efe4998043e67bca778e174eb1a50fb8b2
Mykyta Makhorin <nikitos2159@gmail.com>
2024-03-30 01:13:57 +0100

    update mongodb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,16 @@
 import NextAuth from 'next-auth';
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import GoogleProvider from 'next-auth/providers/google';
+import clientPromise from "../../../lib/mongodb"
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+// dotenv.config();
 
-
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
-});
+export default NextAuth ({
+    // Configure one or more authentication providers
+    providers: [
+      GoogleProvider({
+          clientId: process.env.GOOGLE_ID,
+          clientSecret: process.env.GOOGLE_SECRET,
+      }),
+    ],
+    adapter: MongoDBAdapter(clientPromise)
+  });
