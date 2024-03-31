d7b18a32cbcdc9529b1f008e6ad67b0bf95d824c
Mykyta Makhorin <nikitos2159@gmail.com>
2024-03-31 03:41:09 +0200

    update frontend in Login page

--- components/LoginMenu.jsx
+++ components/LoginMenu.jsx
@@ -16,59 +16,60 @@ function LoginMenu() {
 
             <div className="h-screen w-auto">
                 <div className="flex justify-start items-start">
-                    <div className=" h-screen w-auto  bg-transparent shadow-2xl rounded-r-2xl pl-5 xs:pl-2 md:pr-3">
+                    <div className=" h-screen w-auto  bg-transparent custom-shadow_2 rounded-r-2xl pl-5 xs:pl-2 md:pr-3">
+                        <div>
+                            <div className="flex flex-col text-center items-center gap-7 md:grid grid-rows-5 md:text-xltext-[#000000]">
 
-                        <div className="flex flex-col text-center items-center text-[12px] gap-7 sm:relative md:grid grid-rows-5 md:text-xl w-auto  text-[#000000]">
-
-                            <Link href={'/'}>
-                                <div className="px-4 row-start-1 md:flex items-center">
-                                    <img src="/icons/login.png" className="p-2" style={{ width: '50px', height: "50px" }} />
-                                    <div className="hidden md:block">
-                                        {session.user.email}
+                                <Link href={'/'}>
+                                    <div className="px-4 row-start-1 md:flex items-center">
+                                        <img src="/icons/login.png" className="p-2" style={{ width: '50px', height: "50px" }} />
+                                        <div className="hidden md:block font-bold">
+                                            {session.user.email}
+                                        </div>
+                                        <div className="sm:block md:hidden font-bold">
+                                            Account
+                                        </div>
                                     </div>
-                                    <div className="sm:block md:hidden">
-                                        Account
+                                </Link>
+
+                                <Link href={'/'} className={ActiveLink}>
+                                    <div className=" px-4 row-start-2 md:flex items-center " >
+                                        <img src="/icons/bashboard.png" className="p-2" style={{ width: '50px', height: "50px" }} />
+                                        Dashboard
                                     </div>
-                                </div>
-                            </Link>
-
-                            <Link href={'/'} className={ActiveLink}>
-                                <div className=" px-4 row-start-2 md:flex items-center " >
-                                    <img src="/icons/bashboard.png" className="p-2" style={{ width: '50px', height: "50px" }} />
-                                    Dashboard
-                                </div>
-                            </Link>
+                                </Link>
 
 
-                            <Link href={'/'} className={inActiveLink}>
-                                <div className=" px-4 row-start-3 md:flex items-center" >
-                                    <img src="/icons/product.png" className="p-2" style={{ width: '50px', height: "50px" }} />
-                                    Product
-                                </div>
-                            </Link>
+                                <Link href={'/'} className={inActiveLink}>
+                                    <div className=" px-4 row-start-3 md:flex items-center" >
+                                        <img src="/icons/product.png" className="p-2" style={{ width: '50px', height: "50px" }} />
+                                        Product
+                                    </div>
+                                </Link>
 
-                            <Link href={'/'} className={inActiveLink}>
-                                <div className="px-4 row-start-4 md:flex items-center" >
-                                    <img src="/icons/monitor.png" className="p-2" style={{ width: '50px', height: "50px" }} />
-                                    Orders
-                                </div>
-                            </Link>
+                                <Link href={'/'} className={inActiveLink}>
+                                    <div className="px-4 row-start-4 md:flex items-center" >
+                                        <img src="/icons/monitor.png" className="p-2" style={{ width: '50px', height: "50px" }} />
+                                        Orders
+                                    </div>
+                                </Link>
 
-                            <Link href={'/'} className={inActiveLink}>
-                                <div className=" px-4 row-start-5 md:flex items-center" >
-                                    <img src="/icons/settings.png" className="p-2" style={{ width: '50px', height: "50px" }} />
-                                    Settings
-                                </div>
-                            </Link>
+                                <Link href={'/'} className={inActiveLink}>
+                                    <div className=" px-4 row-start-5 md:flex items-center" >
+                                        <img src="/icons/settings.png" className="p-2" style={{ width: '50px', height: "50px" }} />
+                                        Settings
+                                    </div>
+                                </Link>
 
-                            <Link href={'/'} className={inActiveLink}>
-                                <div className=" px-4 row-start-5 md:flex items-center" >
-                                    <img src="/icons/logout.png" className="p-2" style={{ width: '50px', height: "50px" }} />
-                                    <button className="" onClick={() => signOut()}>LogOut</button>
-                                </div>
-                            </Link>
+                                <Link href={'/'} className={inActiveLink}>
+                                    <div className=" px-4 row-start-5 md:flex items-center" >
+                                        <img src="/icons/logout.png" className="p-2" style={{ width: '50px', height: "50px" }} />
+                                        <button className="" onClick={() => signOut()}>LogOut</button>
+                                    </div>
+                                </Link>
 
 
+                            </div>
                         </div>
                     </div>
 
